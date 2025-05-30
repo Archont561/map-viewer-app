c335b330e9af1c0440e9bcd026aba571983b7e65
Eryk Srebrny <esrebrny@onet.eu>
2025-05-30 17:39:29 +0200

    feat: add new map components to enhance user experience

--- frontend/src/js/webComponents.import.ts
+++ frontend/src/js/webComponents.import.ts
@@ -1,2 +1,6 @@
 import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/dist/components/arcgis-zoom";
+import "@arcgis/map-components/dist/components/arcgis-basemap-toggle";
+import "@arcgis/map-components/dist/components/arcgis-home";
 import "@arcgis/map-components/dist/components/arcgis-layer-list";
+import "@arcgis/map-components/dist/components/arcgis-scale-bar";
