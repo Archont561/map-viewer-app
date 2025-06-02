4646fb3c7ffe220df17e2ca1e36f233587c0cf84
Eryk Srebrny <esrebrny@onet.eu>
2025-06-02 17:25:59 +0200

    feat: change workflow to include astro build step

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -8,6 +8,8 @@ const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://archont561.github.io",
+  base: "map-viewer-app",
   vite: {
     plugins: [tailwindcss()],
     server: {
